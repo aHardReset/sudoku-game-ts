9e62edaac04f621f9269c899aa09095e2e24400f
aHardReset <acg.95.mx@gmail.com>
2022-08-21 01:05:38 -0700

    linkedin fixed

--- src/Home.tsx
+++ src/Home.tsx
@@ -15,7 +15,7 @@ function Home () {
       <p>
         {/* Invite the user to visit my linkedin page */ }
         If you want to contact me for professional opportunities or to see my resume,
-        <a href="/resume"> please visit my linkedin page.</a>
+        <a href="https://linkedin.com/in/aarongaribay" target="_blank" rel="noreferrer"> please visit my linkedin page.</a>
       </p>
 
     </div>
